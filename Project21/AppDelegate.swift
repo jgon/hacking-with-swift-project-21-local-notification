b0d8d28729731d008927c8d5f426ccdc2d9b6384
JackG <jacques.gonzalez@me.com>
2016-02-24 15:23:31 +0100

    Added handling of location notification when launching application or when application is already running.

--- Project21/AppDelegate.swift
+++ Project21/AppDelegate.swift
@@ -15,10 +15,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // Called when receiving a local notification.
+        if let launchOptions = launchOptions {
+            if let notification = launchOptions[UIApplicationLaunchOptionsAnnotationKey] as? UILocalNotification {
+                if let userInfo = notification.userInfo {
+                    let customField1 = userInfo["CustomField1"] as! String
+                    print("App launched with local notification: \(customField1)")
+                }
+            }
+        }
         return true
     }
-
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        if let userInfo = notification.userInfo {
+            let customField1 = userInfo["CustomField1"] as! String
+            print("Did receive local notification: \(customField1)")
+        }
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
